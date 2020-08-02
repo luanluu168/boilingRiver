13e786f06cfa5ded48454c9da276492c2939eae3
luanluu168 <>
2020-08-02 12:33:49 -0700

    Update landing page

--- frontend/src/components/pages/landing.js
+++ frontend/src/components/pages/landing.js
@@ -5,8 +5,8 @@ class Landing extends React.Component {
     render() {
         return (
             <div className="landing">
-                <h1>Welcome to BuyKaCloth</h1>
-                <h2 className="mt-2">A place to shop beautiful cloth</h2>
+                <h1 className="d-inline-block">Welcome to <p className="d-inline rainbow-text">BuyKaCloth</p></h1>
+                <h2 className="mt-2">A place to shop beautiful clothing</h2>
                 <Link className="btn btn-primary mt-3 rounded-pill btn-responsive-lg" to="/">Goto Home</Link>
             </div>
         );
