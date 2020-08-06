84903accb2688e406cc6b0ea0afd56617fb90093
luanluu168 <>
2020-08-06 14:27:41 -0700

    Use hook: error & landing

--- frontend/src/components/pages/error.js
+++ frontend/src/components/pages/error.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Error extends React.Component {
-    render() {
-        return (
-            <div className="container pt-3">
-                <div className="alert alert-success text-center mt-3"><strong>Sorry, Page Not Found...</strong></div>
-            </div>
-        );
-    }
+function Error() {
+    return (
+        <div className="container pt-3">
+            <div className="alert alert-success text-center mt-3"><strong>Sorry, Page Not Found...</strong></div>
+        </div>
+    )
 }
 
 export default Error;
--- frontend/src/components/pages/landing.js
+++ frontend/src/components/pages/landing.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Landing extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src="/media/background.png" className="background-img" alt="background-img"></img>
-                <div className="landing">
-                    <h1 className="d-inline-block">Welcome to <p className="d-inline rainbow-text">BoilingRiver</p></h1>
-                    <h2 className="mt-2">A place to shop beautiful clothing</h2>
-                    <Link className="btn btn-primary mt-3 rounded-pill btn-responsive-lg" to="/">Goto Home</Link>
-                </div>
+function Landing() {
+    return (
+        <div>
+            <img src="/media/background.png" className="background-img" alt="background-img"></img>
+            <div className="landing">
+                <h1 className="d-inline-block">Welcome to <p className="d-inline rainbow-text">BoilingRiver</p></h1>
+                <h2 className="mt-2">A place to shop beautiful clothing</h2>
+                <Link className="btn btn-primary mt-3 rounded-pill btn-responsive-lg" to="/">Goto Home</Link>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 export default Landing;
