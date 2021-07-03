8a71532386f6b70967038d7f1ad5c3e150b27ac8
LuanLuu <lluu3@mail.sfsu.edu>
2021-07-03 11:48:04 -0700

    Update footer: style & dynamic year

--- frontend/public/javascripts/javascripts.js
+++ frontend/public/javascripts/javascripts.js
@@ -1,3 +1,13 @@
-function hello() {
-    alert("Hi");
+function getCurrentYear() {
+    const today = new Date();
+    const currentYear = today ? today.getFullYear() : 2020;
+    return currentYear;
 }
+
+function assignYearForFooter() {
+    const footer = document.querySelector('.footer');
+    if(footer) {
+        footer.innerHTML = '&copy; ' + getCurrentYear() + ' All right reserved';
+    }
+}
+assignYearForFooter();
